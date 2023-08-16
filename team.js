2608665482a44f750bd85eec9dded6ef54f87000
Nori Mueni <norimueni@gmail.com>
2023-08-16 22:34:45 +0300

    another commit

--- team.js
+++ team.js
@@ -3,15 +3,19 @@ const sidenav= document.querySelector('.sidenav');
 const close = document.querySelector('.close');
 const navList= document.querySelector('header nav ul');
 const mobile = document.querySelector('.mobile-flex');
+const hero = document.querySelector('.hero');
+
 
 hamburger.addEventListener('click', (e) => {
     sidenav.classList.toggle('active');
     navList.classList.toggle('active');
     mobile.classList.toggle('active');
+    hero.classList.toggle('active');
 
 });
 close.addEventListener('click', (e) => {
     sidenav.classList.remove('active');
     navList.classList.remove('active');
     mobile.classList.remove('active');
+    hero.classList.toggle('active');
 });
